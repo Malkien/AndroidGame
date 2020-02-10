328ad39797c7de716c9825a51134e502d964ebe0
Malkien <kevin1cuenca@gmail.com>
2020-02-10 20:56:25 +0100

    Primeras pinceladas

---
+++ core/src/Personaje/Vago.java
@@ -0,0 +1,58 @@
+package Personaje;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Vago {
+    protected Sprite sprite;
+    private World mundo;
+    private BodyDef propiedadesCuerpo;
+    private Body cuerpo;
+    private FixtureDef propiedadesFisicasCuerpo;
+    public Vago(World m){
+        mundo=m;
+        sprite=new Sprite(new Texture("Personajes/orco/orc_walk.gif"));
+        int anchuraSprite=1; //Anchura y altura se expresan ahora en metros
+        int alturaSprite=1;//Anchura y altura se expresan ahora en metros
+        sprite.setBounds(3,8,
+                anchuraSprite,alturaSprite); //La posición inicial también debe estar en metros
+
+        this.propiedadesCuerpo= new BodyDef(); //Establecemos las propiedades del cuerpo
+        propiedadesCuerpo.type = BodyDef.BodyType.DynamicBody;
+        propiedadesCuerpo.position.set(sprite.getX(), sprite.getY());
+
+        cuerpo = mundo.createBody(propiedadesCuerpo);
+
+        propiedadesFisicasCuerpo = new FixtureDef();
+        propiedadesFisicasCuerpo.shape = new PolygonShape();
+        ((PolygonShape)propiedadesFisicasCuerpo.shape).setAsBox(anchuraSprite/2f, alturaSprite/2f);
+        propiedadesFisicasCuerpo.density = 1f;
+        cuerpo.createFixture(propiedadesFisicasCuerpo);
+
+        sprite.setOrigin(this.sprite.getWidth()/2,
+                this.sprite.getHeight()/2);
+
+
+    }
+
+
+    public void draw(SpriteBatch batch, float parentAlpha) {
+        //Esta cuenta hace falta por lo de la media altura. Ese absurdo cálculo...
+        sprite.setPosition(cuerpo.getPosition().x-sprite.getWidth()/2,cuerpo.getPosition().y-sprite.getHeight()/2);
+        //Sprite quiere la rotación en grados, el cuerpo la da en radianes. Esta constante convierte de uno a otro.
+        sprite.setRotation(MathUtils.radiansToDegrees*cuerpo.getAngle());
+        sprite.draw(batch);
+    }
+}
--- core/src/slayer/simulator/game/SlakerSimulator.java
+++ core/src/slayer/simulator/game/SlakerSimulator.java
@@ -3,31 +3,60 @@ package slayer.simulator.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+import Personaje.Vago;
 
 public class SlakerSimulator extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private World world;
+	private Vago vago;
+	private Box2DDebugRenderer debugRenderer;
+	private SpriteBatch batch;
+	private OrthographicCamera camara;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		batch=new SpriteBatch();
+		world=new World(new Vector2(0,-9.8f),true);
+		vago=new Vago(world);
+		camara=new OrthographicCamera(10,10);
+		this.debugRenderer=new Box2DDebugRenderer();
+
+		BodyDef propiedadesSuelo= new BodyDef(); //Establecemos las propiedades del cuerpo
+		propiedadesSuelo.type = BodyDef.BodyType.StaticBody;
+		Body suelo = world.createBody(propiedadesSuelo);
+		FixtureDef propiedadesFisicasSuelo=new FixtureDef();
+		propiedadesFisicasSuelo.shape = new PolygonShape();
+		((PolygonShape)propiedadesFisicasSuelo.shape).setAsBox(100/2f, 1/2f);
+		propiedadesFisicasSuelo.density = 1f;
+		suelo.createFixture(propiedadesFisicasSuelo);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+		batch.setProjectionMatrix(camara.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		vago.draw(batch,0);
 		batch.end();
+
+		camara.update();
+		debugRenderer.render(world, camara.combined);
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		world.dispose();
 	}
 }
