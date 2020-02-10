68923afbc7b8bf9cc0dd5b776c8e0883048d0d5c
Malkien <kevin1cuenca@gmail.com>
2020-02-10 18:53:17 +0100

    Initial commit

---
+++ android/src/slayer/simulator/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package slayer.simulator.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import slayer.simulator.game.SlakerSimulator;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SlakerSimulator(), config);
+	}
+}
