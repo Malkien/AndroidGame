91a2dcf8e8b4dea8eee5b927f977ef74845f0238
Malkien <kevin1cuenca@gmail.com>
2020-02-25 19:18:29 +0100

    mapaaaa

---
+++ android/assets/mapa/suelojungla.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="suelojungla" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="../jungla/environment/tiles/jungle/tile_jungle_ground_brown.png" width="192" height="192"/>
+</tileset>
